daacfa800d6ae79eb257e0d8434c58d75ef6b4cb
Jabez <160478790+Motari27@users.noreply.github.com>
2024-10-18 18:59:38 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 // Placeholder for future JavaScript functionality
 console.log("Welcome to St. Camillus School of Nursing!");
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
