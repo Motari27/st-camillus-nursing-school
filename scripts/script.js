e1a1cee8a813b0eeae21ddbdb10a740810161fdf
Jabez <160478790+Motari27@users.noreply.github.com>
2024-10-18 22:20:56 +0300

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -48,3 +48,11 @@ window.addEventListener('click', (e) => {
         modal.style.display = 'none';
     }
 });
+const accordionHeaders = document.querySelectorAll('.accordion-header');
+
+accordionHeaders.forEach(header => {
+    header.addEventListener('click', function () {
+        const content = this.nextElementSibling;
+        content.style.display = content.style.display === 'block' ? 'none' : 'block';
+    });
+});
